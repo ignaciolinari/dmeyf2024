8ee1672e83f968002c6cba3f6b1a21b2d5770d17
Ignacio Linari <ignaciolinarim@hotmail.com>
2024-11-24 03:38:40 -0300

    20pcent undersamp

--- src/workflows/b1_workflow_test.r
+++ src/workflows/b1_workflow_test.r
@@ -296,7 +296,7 @@ TS_strategy_base8 <- function( pinputexps )
 
   param_local$future <- c(202108)
 
-  param_local$final_train$undersampling <- 0.02
+  param_local$final_train$undersampling <- 0.2
   param_local$final_train$clase_minoritaria <- c( "BAJA+1", "BAJA+2")
   param_local$final_train$training <- c(
     202106, 202105, 202104, 202103, 202102, 202101, 
